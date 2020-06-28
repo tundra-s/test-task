1bc9b9db649ca027db58822421cac93b54f3712b
tundra-s <tundra-s@mail.ru>
2020-06-28 23:52:57 +0300

    123

--- public/js/c_pt.js
+++ public/js/c_pt.js
@@ -6,14 +6,19 @@
 // 
 
 // Pt.dynamicColumn - сылка на div элемент колонки со школой времени 
+// Pt.dynamicColumnWrap = сылка на div элемент обертки колонки со школой времени ;
+// Pt.graphDream - график формального рабочего дня;
+// Pt.graphReal - график фактического рабочего дня;
 
 function Pt(data, set) {
 	const that = document.createElement('div');
 	
 	// дефолтные параметры для пункта
 	const settings = {
-		timeRangeStart: 0, // 0-23
-		timeRangeEnd: 7, // 1-24
+		timeRangeStart: 8, // 0-23
+		timeRangeEnd: 21, // 1-24
+
+		ignoreGraph: false,
 
 		showTimeInCell: true,
 
@@ -30,9 +35,9 @@ function Pt(data, set) {
 		place: 'def place',
 		role: 'def role',
 		dateStartTimeDream: new Date("1999-08-16T08:00:00.511Z"),
-		dateEndTimeDream: new Date("1999-08-16T18:0000.211Z"),
+		dateEndTimeDream: new Date("1999-08-16T18:00:00.211Z"),
 		dateStartTimeReal: new Date("1999-08-16T08:00:00.511Z"),
-		dateEndTimeReal: new Date("1999-08-16T08:00:00.511Z")
+		dateEndTimeReal: new Date("1999-08-16T18:00:00.211Z")
 	};
 
 	// =======
@@ -74,12 +79,17 @@ function Pt(data, set) {
 					dynamicColumnWrap = document.createElement('div'),
 					ptName = document.createElement('div'),
 					ptPlace = document.createElement('div'),
-					ptRole = document.createElement('div');
+					ptRole = document.createElement('div'),
+					graphDream = document.createElement('div'),
+					graphReal = document.createElement('div');
+					
 
 
 			that.className = 'work-list__column-content-pt flex';
 			staticColumn.className = 'work-list__static-column-pt flex-grow left-block';
 			dynamicColumn.className = 'work-list__dynamic-column-pt grow-this';
+			graphReal.className = 'work-list__statusBarReal';
+			graphDream.className = 'work-list__statusBarDream';
 			
 			ptName.innerText = values.name;
 			ptPlace.innerText = values.place;
@@ -93,7 +103,12 @@ function Pt(data, set) {
 			staticColumn.appendChild(ptRole);
 
 			dynamicColumn.appendChild(dynamicColumnWrap);
+			dynamicColumn.appendChild(graphDream);
+			dynamicColumn.appendChild(graphReal);
 
+
+			that.graphDream = graphDream;
+			that.graphReal = graphReal;
 			that.dynamicColumn = dynamicColumn;
 			that.dynamicColumnWrap = dynamicColumnWrap;
 
@@ -119,14 +134,26 @@ function Pt(data, set) {
 					element.innerText = prettyTime(i, settings.stripTimeGrid < settings.timeRangeEnd - settings.timeRangeStart);
 				}
 				
-				that.dynamicColumn.appendChild(element);
+				that.dynamicColumnWrap.appendChild(element);
 			}
 		}
 
 
 		// изменим масштаб сетки 
 		function changePtGridSize(){
-			const elementSize = calcPtGridElementSize();
+			const 	elementSize = calcPtGridElementSize(),
+					elements = that.dynamicColumnWrap.children;
+			
+			console.log(elements);
+			for(let i = 0; i < elements.length; i++){
+				console.log(elements[i]);
+				elements[i].style.width = elementSize + 'px';
+				elements[i].style.left = elementSize * (i - settings.timeRangeStart) + 'px';
+			}
+			
+			calcGraphReal();
+			calcGraphDream();
+		
 		}
 
 
@@ -134,6 +161,45 @@ function Pt(data, set) {
 		function calcPtGridElementSize(){
 			return settings.dynamicElementSize / (settings.timeRangeEnd - settings.timeRangeStart);
 		}
+
+		// добавим мечту график 
+		function calcGraphDream(){
+			let graphSize,
+				elementSize = calcPtGridElementSize(),
+				hourStart = new Date(values.dateStartTimeDream),
+				hourEnd = new Date(values.dateEndTimeDream);
+				
+				
+			hourStart = hourStart.getHours();
+			hourEnd = hourEnd.getHours();
+				
+			console.log(hourStart);
+			console.log(hourEnd);
+
+			graphSize = hourEnd - hourStart;
+
+			that.graphDream.style.width =  elementSize * graphSize + 'px';
+			that.graphDream.style.left = elementSize * (hourStart - settings.timeRangeStart) + 'px';
+		}
+
+		
+		// добавим реальный график 
+		function calcGraphReal(){
+			let graphSize,
+				elementSize = calcPtGridElementSize(),
+				hourStart = new Date(values.dateStartTimeReal),
+				hourEnd = new Date(values.dateEndTimeReal);
+				
+				
+			hourStart = hourStart.getHours();
+			hourEnd = hourEnd.getHours();
+				
+			graphSize = hourEnd - hourStart;
+
+			that.graphReal.style.width =  elementSize * graphSize + 'px';
+			that.graphReal.style.left = elementSize * (hourStart - settings.timeRangeStart) + 'px';
+		}
+
 
 	// =======
 	// public functions (methods)
@@ -161,7 +227,8 @@ function Pt(data, set) {
 	createPtStructure();
 	createPtGrid();
 
-	changePtGridSize();
+	calcGraphReal();
+	calcGraphDream();
 
 	return that
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,44 @@
 // Данные в дата js
 // Класс пункта таблицы c_pt.js
 // Остальное тут
-
+const pts = [];
 
 window.onload = () => {
 	if(Pt){
-		let pt = new Pt(),
-			parent = document.getElementsByClassName('work-list__column-content')[0];
-
-		console.log(pt);
-
-		parent.appendChild(pt);
+		let parent = document.getElementsByClassName('work-list__column-content')[0];
 		
+		createTableHeader(parent);
+
+		for(let i = 0; i < 10; i++){
+			pts.push(new Pt());
+			parent.appendChild(pts[i]);
+		}
+
 	}else{
 		console.log('Can\'t find PT class');
 	}
+}
+
+
+function createTableHeader(parent){
+	let newPt;
+	let data = {
+		name : 'Имя',
+		place : 'Место работы',
+		role : 'Должность'
+	};
+	let sets = {
+		ignoreGraph : true,
+		showTimeInCell : true
+	};
+
+	newPt = new Pt(data, sets)
+	pts.push(newPt);
+	parent.appendChild(newPt);
+}
+
+function changeSize(x, y){
+	pts.map(e => {
+		e.changeGridSize(x, y);
+	})
 }
